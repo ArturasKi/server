d53f3be7987b29f1b6cbfbfa324dd6ea82fe4a26
Arturito <arturaskisielius91@gmail.com>
2022-07-18 21:33:04 +0300

    Server beggining

---
+++ index.js
@@ -0,0 +1,33 @@
+import { server } from "./lib/server.js";
+
+const app = {};
+
+app.init = () => {
+    // ETAPAI, kaip pasileidinėja projektas:
+    // 1.
+    // sukurti pradinius folder'ius;
+    // sukurti pradinius file'us;
+    // 2.
+    // prisijungti prie duomenų bazės;
+    // paleisti mūsų serverį (perduoti DB connection ir t.t.);
+    server.init();
+    // 3.
+    // pasikartojantys procesai:
+    // - ištrinti nenaudojamus file'us;
+    // - suzip'inti seną informaciją;
+    // - atsinaujinti API informaciją (valiutos, orai ir t.t.);
+}
+
+app.init(); // kaskart restartavus pasileidžia funkcija;
+
+export { app };
+
+// PASTABOS
+
+// module.export = app;
+
+// Unexpected token 'export' - token programinis žodis, kuris nesupranta, kas yra export;
+
+//  "description": "",
+// "type": "module", - prirašome package.json'e, kad būtų galima naudoti modernią sintaksę export { app };
+// "main": "index.js",
